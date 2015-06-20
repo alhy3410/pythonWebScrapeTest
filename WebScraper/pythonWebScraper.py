b3b9b1ca725f184bf3787c01ebb5b494d6f2f5a5
Andrew Lee <andrewlhy@gmail.com>
2015-06-20 14:25:01 -0700

    grabs data from tables

--- WebScraper/pythonWebScraper.py
+++ WebScraper/pythonWebScraper.py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.w3schools.com/html/html_tables.asp'
+url = 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s'
 response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-print(soup.prettify().encode('cp850',errors='ignore'))
 
+# print(soup.prettify().encode('cp850',errors='ignore'))
+
+print soup.find_all('tr')
 # import requests
 #
 # url = 'http://www.w3schools.com/html/html_tables.asp'
