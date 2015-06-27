92384ab003ed3228eb022ac7f43b0b2b703879dc
Andrew Lee <andrewlhy@gmail.com>
2015-06-27 10:45:56 -0700

    twitterBot python to update all data for auroras

--- WebScraper/pythonWebScrapertesting.py
+++ WebScraper/pythonWebScrapertesting.py
@@ -1,27 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import twitter
+import time
 
-url1 = "http://www.gi.alaska.edu/AuroraForecast/NorthAmerica"
-response = requests.get(url1)
+api = twitter.Api(consumer_key='',
+    consumer_secret='',
+    access_token_key='-',
+    access_token_secret='')
+
+url = "http://www.gi.alaska.edu/AuroraForecast/NorthAmerica"
+response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
 
 # print(soup.prettify().encode('cp850',errors='ignore'))
 print '\n'
-print soup.findAll('div', {"class" : "levels"})
-
+# print soup.findAll('div', {"class" : "levels"})
 levels = soup.findAll('div', {"class" : "levels"})
 
-print '\n'
-print soup.findAll('div', {"class" : "image"})
+levnum = str(levels[0])
+lev = re.findall(r'\a*l">(.+?)\</span>', levnum)
+num = re.findall(r'\a*n">(.+?)\</span>', levnum)
 
+# lev = re.sub('<[^>]*>', '', levnum)
+print '\n'
+
+## For the Image
+# print soup.findAll('div', {"class" : "image"})
 image = soup.findAll('div', {"class" : "image"})
-print '\n'
-
 x=str(image[0])
 matches= re.findall(r'\"/sites(.+?)\"', x)
+finalSite = "http://www.gi.alaska.edu/sites" + matches[0]
+# print matches
+# print finalSite
 
-print matches
-print "http://www.gi.alaska.edu/sites" + matches[0]
+
+## For the time
+timestamp = "Aurora Forecast for "  + time.strftime("%x")
+result = " " + lev[0] + num[0] + "/9. Link: "
+
+tweetIt = timestamp + result + url
+# print tweetIt
+
+status = api.PostUpdate('tweetIt')
+print status.text
